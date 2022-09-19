1be522026b4e8a815e0e713c885d95e0819a515b
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-09-19 06:48:08 +0200

    didn't speed things up

--- mouselab/exact_utils.py
+++ mouselab/exact_utils.py
@@ -23,13 +23,8 @@ def timed_solve_env(
              info contains the number of times Q and V were called
                 as well as the elapsed time ("time")
     """
-    if save_q is True:
-        q_dictionary={}
-    else:
-        q_dictionary=None
-        
     with Timer() as t:
-        Q, V, pi, info = solve(env, q_dictionary=q_dictionary,**solve_kwargs)
+        Q, V, pi, info = solve(env, **solve_kwargs)
         info["time"] = t.elapsed
         if verbose:
             optimal_value = sum(
@@ -43,29 +38,36 @@ def timed_solve_env(
                 V(s)
 
         #  Save Q function
-        if save_q is not None:
-            if ground_truths is not None:
-                # In some cases, it is too costly to save whole Q function
-                all_possible_states = get_all_possible_states_for_ground_truths(
-                    env, ground_truths
-                )
-
-                sa_pairs = get_sa_pairs_from_states(all_possible_states)
-
-                if env.include_last_action:
-                    sa_pairs = add_extended_state_to_sa_pairs(sa_pairs)
-            else:
-                sa_pairs = get_all_possible_sa_pairs_for_env(env, verbose=verbose)
-
-            sa_pairs = sorted(sa_pairs,
-                              key=lambda sa_pair: len([node for node in sa_pair[0] if hasattr(node, 'sample')]))
-
-            if len(set(q_dictionary.keys()).intersection(set(sa_pairs))) < len(set(sa_pairs)):
-                [Q(*sa_pair) for sa_pair in sa_pairs if sa_pair not in set(q_dictionary.keys())]
-
-            info["q_dictionary"] = {key: q_dictionary[key] for key in set(sa_pairs) & set(q_dictionary.keys())}
-
-            info["q_dictionary"] = q_dictionary
+        if save_q is not None and ground_truths is not None:
+            # In some cases, it is too costly to save whole Q function
+            info["q_dictionary"] = construct_partial_q_dictionary(Q, env, ground_truths)
+        elif save_q is not None:
+            info["q_dictionary"] = construct_q_dictionary(Q, env, verbose)
 
     return Q, V, pi, info
 
+
+def construct_q_dictionary(Q, env, verbose=False):
+    """
+    Construct q dictionary for env, given environment is solved
+    """
+
+    sa = get_all_possible_sa_pairs_for_env(env, verbose=verbose)
+    q_dictionary = {pair: Q(*pair) for pair in sa}
+    return q_dictionary
+
+
+def construct_partial_q_dictionary(Q, env, selected_ground_truths):
+    """
+    Construct q dictionary for only specified ground truth values
+    """
+    all_possible_states = get_all_possible_states_for_ground_truths(
+        env, selected_ground_truths
+    )
+    sa = get_sa_pairs_from_states(all_possible_states)
+    
+    if env.include_last_action:
+        sa = add_extended_state_to_sa_pairs(sa)
+        
+    q_dictionary = {pair: Q(*pair) for pair in sa}
+    return q_dictionary
